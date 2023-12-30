458baabc18b56f82a65467c640fecf2b0e57258a
Nguyễn Tấn Tài <ngtantai48@gmail.com>
2023-12-30 13:44:38 +0700

    Render data from DB to view

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,20 @@
-let getHomePage = (req, res) => {
-    return res.render("homepage.ejs");
-}
+import db from "../models/index";
+
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+
+    return res.render("homepage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 let getAboutPage = (req, res) => {
-    return res.render("test/about.ejs");
-}
+  return res.render("test/about.ejs");
+};
 
 // object: {
 //     key: '',
@@ -12,6 +22,6 @@ let getAboutPage = (req, res) => {
 // }
 
 module.exports = {
-    getHomePage: getHomePage,
-    getAboutPage: getAboutPage
-}
+  getHomePage: getHomePage,
+  getAboutPage: getAboutPage,
+};
