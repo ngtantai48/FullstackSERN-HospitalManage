dd45910f7a7b8193493b7ef704ee9aca009cf3df
Nguyễn Tấn Tài <ngtantai48@gmail.com>
2023-12-30 11:42:50 +0700

    Write controller, create ejs view, Configure according to MVC

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,13 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 let router = express.Router();
 
 let initWebRouters = (app) => {
-    router.get("/", (req,res) => {
-        return res.send("Hello world with ngtantai48");
-    });
+    router.get("/", homeController.getHomePage);
+    router.get("/about", homeController.getAboutPage);
+
+    //rest api
     return app.use("/", router);
 }
 
