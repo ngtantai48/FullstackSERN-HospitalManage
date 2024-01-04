de101f6fdfd9eb58f4433332224c21798a4878f8
Nguyễn Tấn Tài <ngtantai48@gmail.com>
2024-01-05 03:41:02 +0700

    Display data to View using ejs loop syntax

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -32,7 +32,9 @@ let displayGetCRUD = async (req, res) => {
   console.log("---------------------------");
   console.log(data);
   console.log("---------------------------");
-  return res.send("display get crud from controller");
+  return res.render("displayCRUD.ejs", {
+    dataTable: data
+  });
 }
 
 module.exports = {
