d48105a6be1af512adfd8fc3486104418dfef92c
Nguyễn Tấn Tài <ngtantai48@gmail.com>
2024-01-07 01:23:35 +0700

    Render with JSX

--- Frontend-ReactJS/src/containers/Auth/Login.js
+++ Frontend-ReactJS/src/containers/Auth/Login.js
@@ -3,19 +3,61 @@ import { connect } from 'react-redux';
 import { push } from "connected-react-router";
 import * as actions from "../../store/actions";
 import './Login.scss';
+// eslint-disable-next-line no-unused-vars
 import { FormattedMessage } from 'react-intl';
 
 
 
 class Login extends Component {
+    // eslint-disable-next-line no-useless-constructor
     constructor(props) {
         super(props);
     }
 
 
     render() {
+        //JSX
         return (
-            <div>Hello Login</div>
+            <div className='login-background'>
+                <div className='login-container'>
+                    <div className='login-content row'>
+                        <div className='col-12 login-text'>
+                            Login
+                        </div>
+                        <div className='col-12 form-group login-input'>
+                            <label>
+                                UserName:
+                            </label>
+                            <input type='text' className='form-control' placeholder='Enter your username ...'></input>
+                        </div>
+                        <div className='col-12 form-group login-input'>
+                            <label>
+                                Password:
+                            </label>
+                            <input type='password' className='form-control' placeholder='Enter your password ...'></input>
+                        </div>
+                        <div className='col-12'>
+                            <button className='btn-login'>
+                                Login
+                            </button>
+                        </div>
+                        <div className='col-12'>
+                            <span className='forgot-password'>
+                                Forgot your password?
+                            </span>
+                        </div>
+                        <div className='col-12 text-center mt-4'>
+                            <span className='text-other-login'>
+                                Or Login with:
+                            </span>
+                        </div>
+                        <div className='col-12 social-login'>
+                            <i className="fab fa-google-plus-g google"></i>
+                            <i className='fab fa-facebook-f facebook'></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
