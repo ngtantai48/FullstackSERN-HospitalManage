627d9ccc12fd4e364fcf4d425189044ec235a3d9
Nguyễn Tấn Tài <ngtantai48@gmail.com>
2024-01-07 02:40:22 +0700

    Init State, handle onChange and onClick events, icon hide/show password.

--- Frontend-ReactJS/src/containers/Auth/Login.js
+++ Frontend-ReactJS/src/containers/Auth/Login.js
@@ -9,11 +9,39 @@ import { FormattedMessage } from 'react-intl';
 
 
 class Login extends Component {
-    // eslint-disable-next-line no-useless-constructor
     constructor(props) {
         super(props);
+        this.state = {
+            username: '',
+            password: '',
+            isShowPassword: false
+        }
     }
 
+    handleOnChangeUsername = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+        console.log(event.target.value)
+    }
+
+    handleOnChangePassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+        console.log(event.target.value)
+    }
+
+    handleLogin = () => {
+        console.log('username: ', this.state.username, ' password: ', this.state.password)
+        console.log('all state ', this.state)
+    }
+
+    handleShowHidePassword = (event) => {
+        this.setState({
+            isShowPassword: !this.state.isShowPassword
+        })
+    }
 
     render() {
         //JSX
@@ -25,31 +53,38 @@ class Login extends Component {
                             Login
                         </div>
                         <div className='col-12 form-group login-input'>
-                            <label>
-                                UserName:
-                            </label>
-                            <input type='text' className='form-control' placeholder='Enter your username ...'></input>
+                            <label>UserName:</label>
+                            <input
+                                className='form-control'
+                                type='text'
+                                placeholder='Enter your username ...'
+                                value={this.state.username}
+                                onChange={(event) => this.handleOnChangeUsername(event)}>
+                            </input>
                         </div>
                         <div className='col-12 form-group login-input'>
-                            <label>
-                                Password:
-                            </label>
-                            <input type='password' className='form-control' placeholder='Enter your password ...'></input>
+                            <label>Password:</label>
+                            <div className='custom-input-password'>
+                                <input
+                                    className='form-control'
+                                    type={this.state.isShowPassword ? 'text' : 'password'}
+                                    placeholder='Enter your password ...'
+                                    value={this.state.password}
+                                    onChange={(event) => this.handleOnChangePassword(event)}>
+                                </input>
+                                <span onClick={(event) => this.handleShowHidePassword(event)}>
+                                    <i class={this.state.isShowPassword ? 'fas fa-eye-slash' : 'fas fa-eye'}></i>
+                                </span>
+                            </div>
                         </div>
                         <div className='col-12'>
-                            <button className='btn-login'>
-                                Login
-                            </button>
+                            <button className='btn-login' onClick={(event) => this.handleLogin(event)}>Login</button>
                         </div>
                         <div className='col-12'>
-                            <span className='forgot-password'>
-                                Forgot your password?
-                            </span>
+                            <span className='forgot-password'>Forgot your password?</span>
                         </div>
                         <div className='col-12 text-center mt-4'>
-                            <span className='text-other-login'>
-                                Or Login with:
-                            </span>
+                            <span className='text-other-login'>Or Login with:</span>
                         </div>
                         <div className='col-12 social-login'>
                             <i className="fab fa-google-plus-g google"></i>
