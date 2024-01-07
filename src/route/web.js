a0249da911cef87b77e7708fccf5d1c3f34547fb
Nguyễn Tấn Tài <ngtantai48@gmail.com>
2024-01-07 21:15:23 +0700

    Write a server-side login API, for the purpose of user authentication. Test API with Postman.

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,7 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
+
 
 let router = express.Router();
 
@@ -13,6 +15,8 @@ let initWebRouters = (app) => {
     router.get("/edit-crud", homeController.getEditCRUD);
     router.post("/put-crud", homeController.putCRUD);
     router.get("/delete-crud", homeController.deleteCRUD);
+
+    router.post('/api/login', userController.handleLogin);
 
     //rest api
     return app.use("/", router);
