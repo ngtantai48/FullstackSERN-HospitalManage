b85d3275ef03b4f3366bbb6deaaa6985db6cce23
Nguyễn Tấn Tài <ngtantai48@gmail.com>
2024-01-08 13:03:15 +0700

    Write Node.js server-side api to prepare for client-side React.

--- src/route/web.js
+++ src/route/web.js
@@ -17,6 +17,7 @@ let initWebRouters = (app) => {
     router.get("/delete-crud", homeController.deleteCRUD);
 
     router.post('/api/login', userController.handleLogin);
+    router.get('/api/get-all-users', userController.handleGetAllUsers);
 
     //rest api
     return app.use("/", router);
